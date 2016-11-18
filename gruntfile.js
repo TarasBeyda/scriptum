585f075f27a51756e1f82e1bd5a3318f47c0185b
Taras Beyda <tarasbeyda@gmail.com>
2016-11-19 00:55:13 +0300

    grunt-contrib-cssmin

---
+++ gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        concat: {
+            options: {
+                separator: ';'
+            }
+            , dist: {
+                src: ['css/*.css', '!ie.css', '!main.css']
+                , dest: 'css/main.css'
+            }
+        }
+        , cssmin: {
+            my_target: {
+                files: [{
+                    expand: true,
+                    cwd: 'css/',
+                    src: ['main.css'],
+                    dest: 'css/',
+                    ext: '.min.css'
+                }]
+            }
+        }
+    });
+    
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    
+    grunt.registerTask('default', ['concat','cssmin']);
+};
